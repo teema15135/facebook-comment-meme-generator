5c81a8421cbb79578dfa9275642966a17d8bec32
Teema Krub <tm@odds.team>
2023-08-06 02:03:53 +0700

    New UI and Implement profile picture and display name

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,13 @@
+const timeoutJobs = {}
+
+function debounce(callback, tag, millis) {
+    if (timeoutJobs[tag] != null) {
+        clearTimeout(timeoutJobs[tag])
+    }
+
+    timeoutJobs[tag] = setTimeout(callback, millis)
+}
+
 function takeshot(canvasCallback) {
     let div = getMainDiv()
     html2canvas(div).then(canvasCallback)
@@ -20,31 +30,88 @@ function takeshotNewTab() {
 
 function renderOuputImage() {
     takeshot((canvas) => {
-        let outputElement = document.getElementById('output')
-        outputElement.src = canvas.toDataURL("image/png")
+        getOutputRenderElement().src = canvas.toDataURL("image/png")
     })
 }
 
-function updateText() {
-    let input = getTextInputElement().value
-    if (input.trim().length == 0) input = "เพ้อเจ้อ"
-    let commentTextSpan = document.getElementById('comment-text')
-    commentTextSpan.innerText = input
+function update() {
+    let displayNameText = getDisplayNameInputElement().value
+    let commentText = getCommentTextInputElement().value
+
+    if (isTextBlank(displayNameText)) displayNameText = "Somchaiyz"
+    if (isTextBlank(commentText)) commentText = "เพ้อเจ้อ"
+
+    let displayNameSpan = getDisplayNameElement()
+    let commentTextSpan = getCommentTextElement()
+
+    displayNameSpan.innerText = displayNameText
+    commentTextSpan.innerText = commentText
 }
 
-function registerTextInputListener() {
-    let textInputElement = getTextInputElement()
-    textInputElement
-        .addEventListener("input", (event) => {
-            updateText()
-            renderOuputImage()
+function registerInputListener() {
+    getCommentTextInputElement()
+        .addEventListener("input", updateAndRenderWithDebounce)
+    getDisplayNameInputElement()
+        .addEventListener("input", updateAndRenderWithDebounce)
+    getProfilePictureInputElement()
+        .addEventListener("change", (event) => {
+            const reader = new FileReader()
+            reader.readAsDataURL(event.target.files[0])
+            reader.onload = function () {
+                updateProfilePicture(reader.result)
+                updateAndRenderWithDebounce()
+            }
         })
+}
+
+function updateProfilePicture(content) {
+    getProfilePictureElement().src = content
+}
+
+function updateAndRender() {
+    update()
+    renderOuputImage()
+}
+
+function updateAndRenderWithDebounce() {
+    update()
+    debounce(() => {
+        renderOuputImage()
+    }, 'render-image', 1000)
+}
+
+function isTextBlank(text) {
+    return text.trim().length == 0
+}
+
+function getOutputRenderElement() {
+    return document.getElementById('output-render')
 }
 
 function getMainDiv() {
     return document.getElementById('main')
 }
 
-function getTextInputElement() {
+function getProfilePictureInputElement() {
+    return document.getElementById('picture-input')
+}
+
+function getProfilePictureElement() {
+    return document.getElementById('profile-picture')
+}
+
+function getDisplayNameInputElement() {
+    return document.getElementById('input-display-name')
+}
+
+function getDisplayNameElement() {
+    return document.getElementById('display-name')
+}
+
+function getCommentTextInputElement() {
     return document.getElementById('input-comment-text')
+}
+
+function getCommentTextElement() {
+    return document.getElementById('comment-text')
 }
